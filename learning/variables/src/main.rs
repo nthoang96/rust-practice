6e9d3efb22cd74a4af7ce00813a146792196b10d
nthoang96 <hoanglion.it@gmail.com>
2024-06-04 23:47:13 +0700

    [updated] add more example into variables project

--- learning/variables/src/main.rs
+++ learning/variables/src/main.rs
@@ -3,4 +3,15 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    let x2 = 5;
+
+    let x2 = x2 + 1;
+
+    {
+        let x2 = x2 * 2;
+        println!("The value of x in the inner scope is: {x2}");
+    }
+
+    println!("The value of x is: {x2}");
 }
